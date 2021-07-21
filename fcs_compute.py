21f0533f4029276960491459b66296ce00acc250
vianneydubois <vianneydubois@hotmail.fr>
2021-07-21 12:39:58 +0200

    runs, no post processing yet

--- fcs_compute.py
+++ fcs_compute.py
@@ -7,24 +7,22 @@ import jsbsim_run as jsbsim
 import os
 
 
-# aeromatic run : function
-
-
-def compute(desired_script_name, init_file_name, aeromatic_script_path, xml_file_path, aircraft_name, aeromatic_path):
+def compute(desired_script_name, init_file_name, xml_file_path, aircraft_name):
     # 1 : run aeromatic to generate the JSBSim aircraft config file
-    # aeromatic_xml.generate_script(aeromatic_script_path, xml_file_path, aircraft_name)
-    # aeromatic_edit.edit_aeromatic_script(aeromatic_script_path, aircraft_name)
-    # aeromatic.aeromatic_run(aeromatic_script_path, aeromatic_path)
+    aeromatic_script_path = os.path.join('resources', 'aeromatic_script.txt')
+    aeromatic_xml.generate_script(aeromatic_script_path, xml_file_path, aircraft_name)
+    aeromatic_edit.edit_aeromatic_script(aeromatic_script_path, aircraft_name)  # useless?
+    aeromatic.aeromatic_run(aeromatic_script_path, 'resources/executables/aeromatic')
 
     # 2 : run AVL
-    #avl.generate_geometry('resources/geom.avl', 'avl_gen_files/gen_geom.avl', xml_file_path)
-    #avl.edit_avl_session('avl_gen_files/session.txt', 'avl_gen_files/gen_geom.avl', 'avl_gen_files/out.txt', 0.8)
-    #avl.run_avl('resources/executables/avl335', 'avl_gen_files/session.txt',
-    #            'avl_gen_files/gen_geom.avl', 'avl_gen_files/out.txt')
+    avl.generate_geometry('resources/geom.avl', 'avl_gen_files/gen_geom.avl', xml_file_path)
+    avl.edit_avl_session('avl_gen_files/session.txt', 'avl_gen_files/gen_geom.avl', 'avl_gen_files/out.txt', 0.8)
+    avl.run_avl('resources/executables/avl335', 'avl_gen_files/session.txt',
+                'avl_gen_files/gen_geom.avl', 'avl_gen_files/out.txt')
 
     # 3 : integrate AVL coefficients in JSBSim aircraft config file
     jsbsim_config_path = os.path.join('aircraft', aircraft_name, aircraft_name + '.xml')
-    #avl_jsbsim.integrate_avl_coef('avl_gen_files/out.txt', jsbsim_config_path)
+    avl_jsbsim.integrate_avl_coef('avl_gen_files/out.txt', jsbsim_config_path)
 
     # 4 : run JSSim
     jsbsim.copy_script_file(desired_script_name, aircraft_name)
@@ -36,9 +34,7 @@ def compute(desired_script_name, init_file_name, aeromatic_script_path, xml_file
     return None
 
 
-aeromatic_script_path = os.path.join('resources', 'aeromatic_script.txt')
-aeromatic_path = os.path.join('resources', 'executables', 'aeromatic')
 aircraft_name = 'new_cessna'
 aircraft_xml_source = 'resources/data/aircraft.xml'
 
-compute('roll', 'airborne', aeromatic_script_path, aircraft_xml_source, aircraft_name, aeromatic_path)
+compute('roll', 'airborne', aircraft_xml_source, aircraft_name)
